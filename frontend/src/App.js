d5aee554ca2bdb43678656d533b662fe463376d6
anarotgo <anarotgo@yahoo.com>
2024-09-30 17:34:31 -0700

    Setup some basic frontend and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-    const [ message, setMessage ] = useState('');
     
-    useEffect( () => {
-        fetch('http://localhost:5000/api')
-            .then( response => response.json() )
-            .then( data => setMessage( data.message ) );
-    }, [] );
-
     return (
-        <div className='App'>
-            <h1>{message}</h1>
+        <div className="App">
+            {/* Header */}
+            <header className='container'>
+                <span id='title'>To-Done</span>
+            </header>
+
+            <div className='container' id='tasks-container'>
+                <span id='header'>Tasks</span>
+                {/* Populate with tasks from backend here */}
+            </div>
         </div>
     );
 }
