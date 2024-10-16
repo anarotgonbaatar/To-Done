990bc61477f2507055514035affa71d17607f455
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2024-10-16 13:38:29 -0700

    changed port to 5000 for backend so it matches front end calls

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.put( '/api/tasks/:id', async ( req, res ) => {
 
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => {
     console.log('Server is running on port ' + PORT + '.');
 })
