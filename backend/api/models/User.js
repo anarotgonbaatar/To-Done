4e746f2055dc2e74606d5ba08564d426eb4c3c55
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2024-10-17 18:07:35 -0700

    Added email, and resetTokens to User model

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -3,6 +3,9 @@ const Task = require("./Task")
 
 //User will own an array of Tasks that are also a mongodb schema
 const userSchema = new mongoose.Schema({
+  email: {type: String, required: true},
+  resetToken: String,
+  restTokenExpiration: Date,
   userName: {type: String, required: true},
   password: {type: String, required: true},
   tasks: [{type: mongoose.Types.ObjectId, ref: 'Task'}]
