5fe1ca9c535cc84f1eb6b31b9e322703bda32344
scriptingdubbo <70049887+scriptingdubbo@users.noreply.github.com>
2024-10-20 09:34:10 -0700

    Added Delete functions for routes

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const {
   getUserTasks,
   createTask,
+  deleteTask,
   createUser,
+  deleteUser,
 } = require('../controller/controller.js');
 const {
   checkUserExist,
@@ -22,6 +24,8 @@ apiRoute.post('/tasks', verifyToken, createTask);
 apiRoute.post('/createUser', checkUserExist, encryptPassword, createUser);
 
 // Delete
+apiRoute.delete('/tasks/:id', verifyToken, deleteTask)
+apiRoute.delete('/removeUser/:id', verifyToken, deleteUser)
 
 module.exports = {
   apiRoute,
