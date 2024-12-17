5de14f743143cb0b0022212c10faacbb4217059a
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2024-12-16 17:02:38 -0800

    added back localhost:3000 for origin

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose
   .catch((err) => console.error('Error connecting to MongoDB'));
 
 // Middleware
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 // Allows requests from React frontend
 app.use(express.json()); // Parses JSON request bodies
 app.use(cookieParser()); // Allows us to access cookies from requests
